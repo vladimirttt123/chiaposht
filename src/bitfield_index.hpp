b391002eab29088e82c40ccbdbcae0f46b624e23
vladimirttt123 <vladimir@commugen.com>
2023-05-26 07:30:42 +0100

    Half bitfield_index once again

--- src/bitfield_index.hpp
+++ src/bitfield_index.hpp
@@ -20,7 +20,8 @@
 struct bitfield_index
 {
     // Cache the number of set bits every kIndexBucket bits.
-    // For a bitfield of size 2^32, this means a 32 MiB index
+		// For a bitfield of size 2^32, this means a 2*2^22 + 4*2^16 + 8*2^0 ~ 8.5 MiB index
+		// For a bitfield of size 2^38, this means a 2*2^28 + 4*2^22 + 8*2^6 ~ 528 MiB index
 		static inline const uint64_t kIndexBucketBits = 10; // i.e. 1024
 		static inline const uint64_t kIndexBucket = 1UL<<kIndexBucketBits;
 
@@ -38,6 +39,12 @@ struct bitfield_index
 					index_hi[idx>>32] = counter + (idx>0?index_hi[(idx>>32)-1]:0);
 					counter = 0;
 				}
+
+				if( (idx&0xffff) == 0 ){
+					index_mid[idx>>16] = counter + (idx>0?index_mid[(idx>>16)-1]:0);
+					counter = 0;
+				}
+
 				index_lo[idx>>kIndexBucketBits] = counter;
 				int64_t const left = std::min( uint64_t(bitfield_->size()) - idx, kIndexBucket);
 				counter += bitfield_->count(idx, idx + left);
@@ -47,6 +54,7 @@ struct bitfield_index
     std::pair<uint64_t, uint64_t> lookup(uint64_t pos, uint64_t offset) const
     {
 			uint64_t const bucket_lo = pos >> kIndexBucketBits;
+			uint64_t const bucket_mid = pos >> 16;
 			uint64_t const bucket_hi = pos >> 32;
 
 			assert(bucket_lo < size);
@@ -54,9 +62,9 @@ struct bitfield_index
 			assert(pos + offset < uint64_t(bitfield_->size()));
 			assert(bitfield_->get(pos) && bitfield_->get(pos + offset));
 
-			uint64_t const base = index_hi[bucket_hi] + index_lo[bucket_lo];
+			uint64_t const base = index_hi[bucket_hi] + index_mid[bucket_mid] + index_lo[bucket_lo];
 
-			assert( base == bitfield_->count(0, bucket_lo<<kIndexBucketBits ) );
+			//assert( base == (uint64_t)bitfield_->count(0, bucket_lo<<kIndexBucketBits ) );
 
 			int64_t const aligned_pos = pos & ~uint64_t(63);
 
@@ -74,7 +82,8 @@ private:
 		void setSize( uint64_t new_size ){
 			size = align_size( new_size );
 			if( size > allocated_size ){
-				index_lo.reset( new uint32_t[allocated_size = size] );
+				index_lo.reset( new uint16_t[allocated_size = size] );
+				index_mid.reset( new uint32_t[1 + (new_size>>16)] );
 				index_hi.reset( new uint64_t[1 + (new_size>>32)] );
 			}
 		}
@@ -82,6 +91,7 @@ private:
 		bitfield * bitfield_ = nullptr;
 		uint64_t allocated_size = 0, size = 0;
 		std::unique_ptr<uint64_t[]> index_hi;
-		std::unique_ptr<uint32_t[]> index_lo;
+		std::unique_ptr<uint32_t[]> index_mid;
+		std::unique_ptr<uint16_t[]> index_lo;
 };
 
