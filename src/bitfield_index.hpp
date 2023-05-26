dcfa3f105cdad63729cf7d5b55d1650f0ac7f099
vladimirttt123 <vladimir@commugen.com>
2023-05-26 06:59:08 +0100

    Half bitfield_index ram size

--- src/bitfield_index.hpp
+++ src/bitfield_index.hpp
@@ -21,52 +21,48 @@ struct bitfield_index
 {
     // Cache the number of set bits every kIndexBucket bits.
     // For a bitfield of size 2^32, this means a 32 MiB index
-		static inline const uint64_t kIndexBucket = 1024;
-		static inline uint64_t align_size( uint64_t size ) { return (size-1+kIndexBucket)/kIndexBucket; }
+		static inline const uint64_t kIndexBucketBits = 10; // i.e. 1024
+		static inline const uint64_t kIndexBucket = 1UL<<kIndexBucketBits;
 
-		bitfield_index( uint64_t entries_count ) : bitfield_(nullptr)
-			, allocated_size( align_size( entries_count ) ), size(0)
-			, index_( new uint64_t[allocated_size] )
-		{
-
-		}
+		bitfield_index( uint64_t entries_count ) { setSize( entries_count ); }
 
 		bitfield_index(bitfield &b) : bitfield_(&b)
-			, allocated_size( align_size( bitfield_->size() ) ), size(allocated_size)
-			, index_( new uint64_t[allocated_size] )
     {
 			reinit( &b );
 		}
 
 		void reinit( bitfield *b ){
 			bitfield_ = b;
-			size = align_size( bitfield_->size() );
-			if( size > allocated_size )
-				index_.reset( new uint64_t[allocated_size = size] );
+			setSize( b->size() );
 
-			for( uint64_t idx = 0, counter = 0; idx < uint64_t(bitfield_->size()); idx += kIndexBucket) {
-					assert( idx/kIndexBucket < allocated_size );
+			for( uint64_t idx = 0, counter = 0; idx < uint64_t(bitfield_->size()); idx += kIndexBucket ) {
+				assert( (idx >> kIndexBucketBits ) < allocated_size );
 
-					index_[idx/kIndexBucket] = counter;
-					int64_t const left = std::min( uint64_t(bitfield_->size()) - idx, kIndexBucket);
-					counter += bitfield_->count(idx, idx + left);
+				if( (idx&0xffffffff) == 0 ){
+					index_hi[idx>>32] = counter + (idx>0?index_hi[(idx>>32)-1]:0);
+					counter = 0;
+				}
+				index_lo[idx>>kIndexBucketBits] = counter;
+				int64_t const left = std::min( uint64_t(bitfield_->size()) - idx, kIndexBucket);
+				counter += bitfield_->count(idx, idx + left);
 			}
 		}
 
     std::pair<uint64_t, uint64_t> lookup(uint64_t pos, uint64_t offset) const
     {
-        uint64_t const bucket = pos / kIndexBucket;
+			uint64_t const bucket_lo = pos >> kIndexBucketBits;
+			uint64_t const bucket_hi = pos >> 32;
 
-				assert(bucket < size);
+				assert(bucket_lo < size);
 				assert(pos < uint64_t(bitfield_->size()));
 				assert(pos + offset < uint64_t(bitfield_->size()));
 				assert(bitfield_->get(pos) && bitfield_->get(pos + offset));
 
-        uint64_t const base = index_[bucket];
+				uint64_t const base = index_hi[bucket_hi] + index_lo[bucket_lo];
 
         int64_t const aligned_pos = pos & ~uint64_t(63);
 
-				uint64_t const aligned_pos_count = bitfield_->count(bucket * kIndexBucket, aligned_pos);
+				uint64_t const aligned_pos_count = bitfield_->count(bucket_lo << kIndexBucketBits, aligned_pos);
 				uint64_t const offset_count = aligned_pos_count + bitfield_->count(aligned_pos, pos + offset);
 				uint64_t const pos_count = aligned_pos_count + bitfield_->count(aligned_pos, pos);
 
@@ -75,8 +71,19 @@ struct bitfield_index
         return { base + pos_count, offset_count - pos_count };
     }
 private:
+		static inline uint64_t align_size( uint64_t size ) { return (size-1+(1UL<<kIndexBucketBits))>>kIndexBucketBits; }
+
+		void setSize( uint64_t new_size ){
+			size = align_size( bitfield_->size() );
+			if( size > allocated_size ){
+				index_lo.reset( new uint32_t[allocated_size = size] );
+				index_hi.reset( new uint64_t[1 + (new_size>>32)] );
+			}
+		}
+
 		bitfield * bitfield_;
-		uint64_t allocated_size, size;
-		std::unique_ptr<uint64_t[]> index_;
+		uint64_t allocated_size, size = 0;
+		std::unique_ptr<uint64_t[]> index_hi;
+		std::unique_ptr<uint32_t[]> index_lo;
 };
 
