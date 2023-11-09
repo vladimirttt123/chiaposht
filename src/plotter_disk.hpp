90a522f8486cd2c3ff89ab1e8f627999ef056b3c
vladimirttt123 <vladimir@commugen.com>
2023-11-09 15:34:27 +0000

    Decrease expected bitfield size

--- src/plotter_disk.hpp
+++ src/plotter_disk.hpp
@@ -168,15 +168,16 @@ public:
             throw InvalidValueException("Stripe size too large");
         }
 
+				uint64_t expected_bitfield_size = bitfield::memSize( (uint64_t)((1ULL << k)*0.9) );
 				if( phases_flags & ENABLE_BITFIELD ){
-					if( bitfield::memSize( 1ULL << k ) > memory_size ){
+					if(  expected_bitfield_size > memory_size ){
 						std::cout << "WARNING: The expected size of bitfield is bigger than buffer size than disable bitfield." << std::endl
-											<< "Minimum buffer size to use bitfield is " << ((bitfield::memSize( 1ULL << k )>>20) + sub_mbytes) << "MiB." << std::endl;
+											<< "Minimum buffer size to use bitfield is " << ((expected_bitfield_size>>20) + sub_mbytes) << "MiB." << std::endl;
 						phases_flags &= ~ENABLE_BITFIELD;
 					}
-					else if( bitfield::memSize( 1ULL << k )*2 > memory_size ){
+					else if( expected_bitfield_size*2ULL > memory_size ){
 						std::cout << "WARNING: 2 bitfields cannot fit into buffer. One of them expected to flush to disk." << std::endl
-											<< "Minimum buffer size to elemenate this is " << ((bitfield::memSize( 1ULL << k )>>19) + sub_mbytes) << "MiB." << std::endl;
+											<< "Minimum buffer size to elemenate this is " << ((expected_bitfield_size>>19) + sub_mbytes) << "MiB." << std::endl;
 					}
 				}
 
