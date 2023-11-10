3c8fb7cd94e0fa48999f42a84a7dcde9d32c14f2
vladimirttt123 <vladimir@commugen.com>
2023-11-10 11:07:15 +0000

    Read bucket without atomic for small amount of threads.

--- src/sorting_bucket.hpp
+++ src/sorting_bucket.hpp
@@ -123,16 +123,16 @@ struct SortingBucket{
 			statistics_file->Remove();
 		}
 
-		uint32_t buckets_count = 1<<bucket_bits_count_;
-		auto bucket_positions = std::make_unique<uint64_t[]>( buckets_count );
+		uint32_t subbuckets_count = 1<<bucket_bits_count_;
+		auto subbucket_positions = std::make_unique<uint64_t[]>( subbuckets_count );
 
-		// Calculate initial buckets positions.
-		bucket_positions[0] = 0;
-		for( uint32_t i = 1; i < buckets_count; i++ )
-			bucket_positions[i] = bucket_positions[i-1] + (stats[i-1]*entry_size_);
+		// Calculate initial subbuckets positions.
+		subbucket_positions[0] = 0;
+		for( uint32_t i = 1; i < subbuckets_count; i++ )
+			subbucket_positions[i] = subbucket_positions[i-1] + (stats[i-1]*entry_size_);
 
 		// Last position should end at count.
-		assert( bucket_positions[buckets_count-1]/entry_size_ + stats[buckets_count-1] == Count() );
+		assert( subbucket_positions[subbuckets_count-1]/entry_size_ + stats[subbuckets_count-1] == Count() );
 
 
 		auto end_prepare_time = std::chrono::high_resolution_clock::now();
@@ -142,59 +142,87 @@ struct SortingBucket{
 		if( num_read_threads <= 1 || /*read_size*/ Size() < 1024*entry_size_ ){
 
 			for( StreamBuffer buf; disk->Read( buf ) > 0; )
-				 FillBuckets( memory, buf.get(), buf.used(), bucket_positions.get(), entry_size_ );
+				 FillBuckets( memory, buf.get(), buf.used(), subbucket_positions.get(), entry_size_ );
 
-			assert( bucket_positions[0] == stats[0]*entry_size_ ); // check first bucket is full
-			assert( bucket_positions[buckets_count-1]/entry_size_ == Count() ); // check last bucket is full
+			assert( subbucket_positions[0] == stats[0]*entry_size_ ); // check first bucket is full
+			assert( subbucket_positions[subbuckets_count-1]/entry_size_ == Count() ); // check last bucket is full
 		} else { // Read by threads
-			// read in 2 directions one fills from start and second from back.
-			auto a_bucket_positions = std::make_unique<std::atomic_uint64_t[]>( buckets_count );
-			for( uint32_t i = 0; i < buckets_count - 1; i++ ){
-				a_bucket_positions[i].store( bucket_positions[i], std::memory_order_relaxed );
-			}
-			a_bucket_positions[buckets_count-1].store( bucket_positions[buckets_count-1], std::memory_order_relaxed );
+			if( num_read_threads < 6 ){
+				 // read in 2 directions one fills from start and second from back without synchronizations.
+				 auto subbucket_end_positions = std::make_unique<uint64_t[]>( subbuckets_count );
+				 // Calculate initial end subbuckets positions.
+				 for( uint32_t i = 0; i < subbuckets_count-1; i++ )
+					 subbucket_end_positions[i] = subbucket_positions[i+1] - entry_size_;
+				 subbucket_end_positions[subbuckets_count-1] = entry_size_*entries_count - entry_size_;
 
-			// Define thread function
-			auto thread_func = [this, &memory, &a_bucket_positions](){
-				StreamBuffer buf( BUF_SIZE/entry_size_*entry_size_ );
-				std::unique_ptr<IBlockReader> reader( disk->CreateReader() );
+				 auto thread_function = [this, &memory]( uint64_t * positions, int64_t direction){
+					 StreamBuffer buf( BUF_SIZE/entry_size_*entry_size_ );
+					 std::unique_ptr<IBlockReader> reader( disk->CreateReader() );
 
-				while(  reader->Read( buf ) > 0 ){
-					for( auto buf_ptr = buf.get() + buf.used() - entry_size_; buf_ptr >= buf.get(); buf_ptr -= entry_size_ ){
-						auto b_bits = (uint32_t)Util::ExtractNum64( buf_ptr, begin_bits_, bucket_bits_count_ );
-						// Put next entry to its bucket and move pointer inside bucket to next entry position
-						memcpy( memory + a_bucket_positions[b_bits].fetch_add( entry_size_, std::memory_order_relaxed), buf_ptr, entry_size_ );
-					}
-				}
-			};
+					 while(  reader->Read( buf ) > 0 )
+						 FillBuckets( memory, buf.get(), buf.used(), positions, direction );
+				 };
+				 // start threads
+				 std::thread forward_thread( thread_function, subbucket_positions.get(), entry_size_ );
+				 std::thread backward_thread( thread_function, subbucket_end_positions.get(), -entry_size_ );
 
-			{	// Start threads
-				std::vector<std::thread> threads;
-				for( uint32_t t = 0; t < num_read_threads; t++ )
-					threads.emplace_back( thread_func );
-
-				for (auto& t : threads) t.join();
-
-				if( read_mutex != NULL ) read_mutex->unlock(); // for external purposes mark end of reading.
-			}
+				 forward_thread.join();
+				 backward_thread.join();
 
 #ifndef NDEBUG
-			// Chcek everything read as written.
-			for( uint32_t i = 0; i < buckets_count-1; i++ )
-				assert( a_bucket_positions[i].load( std::memory_order_relaxed ) == bucket_positions[i+1] );
-			// last bucket at the count
-			assert( a_bucket_positions[buckets_count-1].load( std::memory_order_relaxed ) == Count()*entry_size_ );
+				 // Chcek everything read as written.
+				 for( uint32_t i = 0; i < subbuckets_count; i++ )
+					 assert( subbucket_positions[i] == subbucket_end_positions[i] + entry_size_ );
 #endif
+			} else {
+				auto a_subbucket_positions = std::make_unique<std::atomic_uint64_t[]>( subbuckets_count );
+				for( uint32_t i = 0; i < subbuckets_count - 1; i++ ){
+					a_subbucket_positions[i].store( subbucket_positions[i], std::memory_order_relaxed );
+				}
+				a_subbucket_positions[subbuckets_count-1].store( subbucket_positions[subbuckets_count-1], std::memory_order_relaxed );
+
+				// Define thread function
+				auto thread_func = [this, &memory, &a_subbucket_positions](){
+					StreamBuffer buf( BUF_SIZE/entry_size_*entry_size_ );
+					std::unique_ptr<IBlockReader> reader( disk->CreateReader() );
+
+					while(  reader->Read( buf ) > 0 ){
+						for( auto buf_ptr = buf.get() + buf.used() - entry_size_; buf_ptr >= buf.get(); buf_ptr -= entry_size_ ){
+							auto b_bits = (uint32_t)Util::ExtractNum64( buf_ptr, begin_bits_, bucket_bits_count_ );
+							// Put next entry to its bucket and move pointer inside bucket to next entry position
+							memcpy( memory + a_subbucket_positions[b_bits].fetch_add( entry_size_, std::memory_order_relaxed), buf_ptr, entry_size_ );
+						}
+					}
+				};
+
+				{	// Start threads
+					std::vector<std::thread> threads;
+					for( uint32_t t = 0; t < num_read_threads; t++ )
+						threads.emplace_back( thread_func );
+
+					for (auto& t : threads) t.join();
+				}
+
+#ifndef NDEBUG
+				// Chcek everything read as written.
+				for( uint32_t i = 0; i < subbuckets_count-1; i++ )
+					assert( a_subbucket_positions[i].load( std::memory_order_relaxed ) == subbucket_positions[i+1] );
+				// last bucket at the count
+				assert( a_subbucket_positions[subbuckets_count-1].load( std::memory_order_relaxed ) == Count()*entry_size_ );
+#endif
+			}
+
 			// Clean underling resources
+			if( read_mutex != NULL ) read_mutex->unlock(); // for external purposes mark end of reading.
 			disk->EndToRead();
 			total_reads = disk->getTotalReads(); // save reads statistics
 			instant_reads = disk->getInstantReads();// save reads statistics
 			disk.reset();
 
 			// Fix bucket_positions for sorting step in such way that bucket_positions[i] reprsents end position of subbucket.
-			bucket_positions[0] = stats[0]*entry_size_;
-			for( uint32_t i = 1; i < buckets_count; i++ )
-				bucket_positions[i] = bucket_positions[i-1] + stats[i]*entry_size_;
+			subbucket_positions[0] = stats[0]*entry_size_;
+			for( uint32_t i = 1; i < subbuckets_count; i++ )
+				subbucket_positions[i] = subbucket_positions[i-1] + stats[i]*entry_size_;
 		}
 
 		read_time = (std::chrono::high_resolution_clock::now() - end_prepare_time)/std::chrono::milliseconds(1);
@@ -202,9 +230,9 @@ struct SortingBucket{
 		if( num_threads <= 1 ){
 			// Sort first bucket
 			QuickSort::Sort( memory, entry_size_, stats[0], begin_bits_ + bucket_bits_count_ );
-			for( uint32_t i = 1; i < buckets_count; i++ ){
-				assert( bucket_positions[i] == (bucket_positions[i-1] + stats[i]*entry_size_) ); // check any bucket is full
-				QuickSort::Sort( memory + bucket_positions[i-1], entry_size_, stats[i], begin_bits_ + bucket_bits_count_ );
+			for( uint32_t i = 1; i < subbuckets_count; i++ ){
+				assert( subbucket_positions[i] == (subbucket_positions[i-1] + stats[i]*entry_size_) ); // check any bucket is full
+				QuickSort::Sort( memory + subbucket_positions[i-1], entry_size_, stats[i], begin_bits_ + bucket_bits_count_ );
 			}
 		}else {
 			// Sort in threads
@@ -213,16 +241,16 @@ struct SortingBucket{
 			memset( sorted_positions, 0, num_threads*8 ); // init for zeros...
 
 			for( uint32_t i = 0; i < num_threads; i++ )
-				threads[i] = std::thread( [&num_threads, &buckets_count, this, &bucket_positions, &memory, &stats, &sorted_positions ]( uint32_t thread_no ){
-						for( uint32_t t = thread_no; t < buckets_count; t += num_threads ){
-							assert( t == 0 || bucket_positions[t] == (bucket_positions[t-1] + stats[t]*entry_size_) ); // check any bucket is full
+				threads[i] = std::thread( [&num_threads, &subbuckets_count, this, &subbucket_positions, &memory, &stats, &sorted_positions ]( uint32_t thread_no ){
+						for( uint32_t t = thread_no; t < subbuckets_count; t += num_threads ){
+							assert( t == 0 || subbucket_positions[t] == (subbucket_positions[t-1] + stats[t]*entry_size_) ); // check any bucket is full
 
-							uint64_t start_pos = t == 0 ? 0 : bucket_positions[t-1];
+							uint64_t start_pos = t == 0 ? 0 : subbucket_positions[t-1];
 
 							QuickSort::Sort( memory + start_pos, entry_size_, stats[t], begin_bits_ + bucket_bits_count_ );
 
 							// now update global sorted position
-							sorted_positions[thread_no] = bucket_positions[t];
+							sorted_positions[thread_no] = subbucket_positions[t];
 
 							if( thread_no == 0 ){ // only 1st thread updates
 
@@ -300,7 +328,7 @@ private:
 		}
 	}
 
-
+	#pragma region CacheBucket support {
 	// This function in particular for CachBucket
 	inline void BulkAdd( StreamBuffer & entries, const uint32_t * stats ){
 		assert( disk ); // Check not closed
@@ -333,6 +361,8 @@ private:
 		BulkAdd( entries, stats );
 	}
 	friend struct CacheBucket;
+	#pragma endregion CacheBucket support }
+
 };
 
 #endif // SRC_CPP_SORTING_BUCKET_HPP_
