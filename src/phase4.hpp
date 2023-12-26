fddccbd26572fe184108555974acff96bfa6fbea
vladimirttt123 <vladimir@commugen.com>
2023-12-26 16:09:51 +0000

    Add thread to P4

--- src/phase4.hpp
+++ src/phase4.hpp
@@ -62,7 +62,7 @@ void RunPhase4(uint8_t k, uint8_t pos_size, FileDisk &tmp2_disk, Phase3Results &
     res.final_table_begin_pointers[10] = begin_byte_C3;
     res.final_table_begin_pointers[11] = end_byte;
 
-    uint64_t plot_file_reader = 0;
+		// uint64_t plot_file_reader = 0;
     uint64_t final_file_writer_1 = begin_byte_C1;
     uint64_t final_file_writer_2 = begin_byte_C3;
     uint64_t final_file_writer_3 = res.final_table_begin_pointers[7];
@@ -81,61 +81,141 @@ void RunPhase4(uint8_t k, uint8_t pos_size, FileDisk &tmp2_disk, Phase3Results &
     std::cout << "\tStarting to write C1 and C3 tables" << std::endl;
 
     ParkBits to_write_p7;
-    const int progress_update_increment = res.final_entries_written / max_phase4_progress_updates;
+		//const int progress_update_increment = res.final_entries_written / max_phase4_progress_updates;
 
-    // We read each table7 entry, which is sorted by f7, but we don't need f7 anymore. Instead,
-    // we will just store pos6, and the deltas in table C3, and checkpoints in tables C1 and C2.
-    for (uint64_t f7_position = 0; f7_position < res.final_entries_written; f7_position++) {
-				auto right_entry_buf = res.table7_sm->ReadEntry(plot_file_reader);
+		std::mutex write_mutex;
 
-        plot_file_reader += right_entry_size_bytes;
-				uint64_t entry_y = Util::SliceInt64FromBytes(right_entry_buf, k);
-        uint64_t entry_new_pos = Util::SliceInt64FromBytes(right_entry_buf, k, pos_size);
+		// We read each table7 entry, which is sorted by f7, but we don't need f7 anymore. Instead,
+		// we will just store pos6, and the deltas in table C3, and checkpoints in tables C1 and C2.
+		res.table7_sm->EnsureSortingStarted();
+		for( bool cont = true ; cont; ){
+			cont = !res.table7_sm->CurrentBucketIsLast();
 
-        Bits entry_y_bits = Bits(entry_y, k);
+			std::thread p7_thread( [&k, &pos_size, &res, &right_entry_size_bytes, &P7_entry_buf,
+														 &P7_park_size, &to_write_p7, &write_mutex, &final_file_writer_3, &tmp2_disk](){
+						for( uint64_t f7_position = 0; f7_position < res.table7_sm->CurrentBucketSize(); f7_position += right_entry_size_bytes ) {
+							auto right_entry_buf = res.table7_sm->CurrentBucketBuffer(/*is providing position could impove?*/) + f7_position;
+							uint64_t entry_new_pos = Util::SliceInt64FromBytes( right_entry_buf, k, pos_size );
 
-        if (f7_position % kEntriesPerPark == 0 && f7_position > 0) {
-            memset(P7_entry_buf, 0, P7_park_size);
-            to_write_p7.ToBytes(P7_entry_buf);
-            tmp2_disk.Write(final_file_writer_3, (P7_entry_buf), P7_park_size);
-            final_file_writer_3 += P7_park_size;
-            to_write_p7 = ParkBits();
-        }
+							if( ( res.table7_sm->CurrentBucketNo() > 0 || f7_position > 0 )
+									&& ((f7_position+res.table7_sm->CurrentBucketStart())/right_entry_size_bytes) % kEntriesPerPark == 0 ) {
+								memset(P7_entry_buf, 0, P7_park_size);
+								to_write_p7.ToBytes(P7_entry_buf);
+								{
+									std::lock_guard<std::mutex> lk(write_mutex);
+									tmp2_disk.Write( final_file_writer_3, (P7_entry_buf), P7_park_size);
+								}
+								final_file_writer_3 += P7_park_size;
+								to_write_p7 = ParkBits();
+							}
 
-        to_write_p7 += ParkBits(entry_new_pos, k + 1);
+							to_write_p7 += ParkBits(entry_new_pos, k + 1);
+						}
+					} );
 
-        if (f7_position % kCheckpoint1Interval == 0) {
-            entry_y_bits.ToBytes(C1_entry_buf);
-            tmp2_disk.Write(final_file_writer_1, (C1_entry_buf), Util::ByteAlign(k) / 8);
-            final_file_writer_1 += Util::ByteAlign(k) / 8;
-            if (num_C1_entries > 0) {
-                final_file_writer_2 = begin_byte_C3 + (num_C1_entries - 1) * size_C3;
-                size_t num_bytes =
-                    Encoding::ANSEncodeDeltas(deltas_to_write, kC3R, C3_entry_buf + 2) + 2;
+			std::thread C_thread([&res, &right_entry_size_bytes, &k, &C1_entry_buf, &final_file_writer_1,
+														&num_C1_entries, &tmp2_disk,&write_mutex, &begin_byte_C3, &final_file_writer_2,
+														&size_C3, &C3_entry_buf, &deltas_to_write, &prev_y, &C2](){
+						for( uint64_t f7_position = 0; f7_position < res.table7_sm->CurrentBucketSize(); f7_position += right_entry_size_bytes ) {
+							auto right_entry_buf = res.table7_sm->CurrentBucketBuffer(/*is providing position could impove?*/) + f7_position;
+							uint64_t entry_y = Util::SliceInt64FromBytes(right_entry_buf, k);
+							Bits entry_y_bits = Bits(entry_y, k);
 
-                // We need to be careful because deltas are variable sized, and they need to fit
-                assert(size_C3 * 8 > num_bytes);
+							if( ((f7_position+res.table7_sm->CurrentBucketStart())/right_entry_size_bytes) % kCheckpoint1Interval == 0) {
+								entry_y_bits.ToBytes(C1_entry_buf);
+								{
+									std::lock_guard<std::mutex> lk(write_mutex);
+									tmp2_disk.Write(final_file_writer_1, (C1_entry_buf), Util::ByteAlign(k) / 8);
+								}
+								final_file_writer_1 += Util::ByteAlign(k) / 8;
+								if (num_C1_entries > 0) {
+									final_file_writer_2 = begin_byte_C3 + (num_C1_entries - 1) * size_C3;
+									size_t num_bytes =
+											Encoding::ANSEncodeDeltas(deltas_to_write, kC3R, C3_entry_buf + 2) + 2;
 
-                // Write the size
-                Util::IntToTwoBytes(C3_entry_buf, num_bytes - 2);
+									// We need to be careful because deltas are variable sized, and they need to fit
+									assert(size_C3 * 8 > num_bytes);
 
-                tmp2_disk.Write(final_file_writer_2, (C3_entry_buf), num_bytes);
-                final_file_writer_2 += num_bytes;
-            }
-            prev_y = entry_y;
-            if (f7_position % (kCheckpoint1Interval * kCheckpoint2Interval) == 0) {
-                C2.emplace_back(std::move(entry_y_bits));
-            }
-            deltas_to_write.clear();
-            ++num_C1_entries;
-        } else {
-            deltas_to_write.push_back(entry_y - prev_y);
-            prev_y = entry_y;
-        }
-        if (flags & SHOW_PROGRESS && f7_position % progress_update_increment == 0) {
-            progress(4, f7_position, res.final_entries_written);
-        }
-    }
+									// Write the size
+									Util::IntToTwoBytes(C3_entry_buf, num_bytes - 2);
+
+									{
+										std::lock_guard<std::mutex> lk(write_mutex);
+										tmp2_disk.Write(final_file_writer_2, (C3_entry_buf), num_bytes);
+									}
+									final_file_writer_2 += num_bytes;
+								}
+								prev_y = entry_y;
+								if( ((f7_position+res.table7_sm->CurrentBucketStart())/right_entry_size_bytes) % (kCheckpoint1Interval * kCheckpoint2Interval) == 0) {
+									C2.emplace_back(std::move(entry_y_bits));
+								}
+								deltas_to_write.clear();
+								++num_C1_entries;
+							} else {
+								deltas_to_write.push_back(entry_y - prev_y);
+								prev_y = entry_y;
+							}
+						}
+					});
+
+			p7_thread.join();
+			C_thread.join();
+			if( cont ) res.table7_sm->SwitchNextBucket();
+		}
+
+		// // We read each table7 entry, which is sorted by f7, but we don't need f7 anymore. Instead,
+		// // we will just store pos6, and the deltas in table C3, and checkpoints in tables C1 and C2.
+		// for (uint64_t f7_position = 0; f7_position < res.final_entries_written; f7_position++) {
+				// auto right_entry_buf = res.table7_sm->ReadEntry(plot_file_reader);
+
+		//     plot_file_reader += right_entry_size_bytes;
+				// uint64_t entry_y = Util::SliceInt64FromBytes(right_entry_buf, k);
+		//     uint64_t entry_new_pos = Util::SliceInt64FromBytes(right_entry_buf, k, pos_size);
+
+		//     Bits entry_y_bits = Bits(entry_y, k);
+
+		//     if (f7_position % kEntriesPerPark == 0 && f7_position > 0) {
+		//         memset(P7_entry_buf, 0, P7_park_size);
+		//         to_write_p7.ToBytes(P7_entry_buf);
+		//         tmp2_disk.Write(final_file_writer_3, (P7_entry_buf), P7_park_size);
+		//         final_file_writer_3 += P7_park_size;
+		//         to_write_p7 = ParkBits();
+		//     }
+
+		//     to_write_p7 += ParkBits(entry_new_pos, k + 1);
+
+		//     if (f7_position % kCheckpoint1Interval == 0) {
+		//         entry_y_bits.ToBytes(C1_entry_buf);
+		//         tmp2_disk.Write(final_file_writer_1, (C1_entry_buf), Util::ByteAlign(k) / 8);
+		//         final_file_writer_1 += Util::ByteAlign(k) / 8;
+		//         if (num_C1_entries > 0) {
+		//             final_file_writer_2 = begin_byte_C3 + (num_C1_entries - 1) * size_C3;
+		//             size_t num_bytes =
+		//                 Encoding::ANSEncodeDeltas(deltas_to_write, kC3R, C3_entry_buf + 2) + 2;
+
+		//             // We need to be careful because deltas are variable sized, and they need to fit
+		//             assert(size_C3 * 8 > num_bytes);
+
+		//             // Write the size
+		//             Util::IntToTwoBytes(C3_entry_buf, num_bytes - 2);
+
+		//             tmp2_disk.Write(final_file_writer_2, (C3_entry_buf), num_bytes);
+		//             final_file_writer_2 += num_bytes;
+		//         }
+		//         prev_y = entry_y;
+		//         if (f7_position % (kCheckpoint1Interval * kCheckpoint2Interval) == 0) {
+		//             C2.emplace_back(std::move(entry_y_bits));
+		//         }
+		//         deltas_to_write.clear();
+		//         ++num_C1_entries;
+		//     } else {
+		//         deltas_to_write.push_back(entry_y - prev_y);
+		//         prev_y = entry_y;
+		//     }
+		//     if (flags & SHOW_PROGRESS && f7_position % progress_update_increment == 0) {
+		//         progress(4, f7_position, res.final_entries_written);
+		//     }
+		// }
     Encoding::ANSFree(kC3R);
     res.table7_sm.reset();
 
